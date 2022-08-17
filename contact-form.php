67f2bcc1a3cb52d07b3de062ecde5330ac41da49
KlausB-ctrl <ryanantjackson@gmail.com>
2022-08-17 20:35:13 +1000

    Adding functionality to contact form.

---
+++ contact-form.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $POST['name'];
+    $mailFrom = $POST['email'];
+    $subject = $POST['subject'];
+    $message = $POST['message'];
+
+    $mailTo = "ryanantjackson@outlook.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an email from ".$name".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html");
+}
+
